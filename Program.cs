fac2bc24984044e491b31a5591beaa3cd9cde8fc
ziadibrahim-1 <Ziadibr2912@gmail.com>
2026-02-05 10:41:36 +0200

    Rectangle with Auto Properties

--- Program.cs
+++ Program.cs
@@ -5,18 +5,28 @@
         static void Main(string[] args)
         {
             #region  Person Object Initializer
-            Person p = new Person
+            //Person p = new Person
+            //{
+            //    FirstName = "John",
+            //    LastName = "Doe",
+            //    Age = 30,
+            //    Address = new Address
+            //    {
+            //        City = "New York",
+            //        Street = "5th Avenue"
+            //    }
+            //};
+            //Console.WriteLine(p);
+            #endregion
+
+            #region Rectangle with Auto Properties
+            Rectangle r = new Rectangle(1)
             {
-                FirstName = "John",
-                LastName = "Doe",
-                Age = 30,
-                Address = new Address
-                {
-                    City = "New York",
-                    Street = "5th Avenue"
-                }
+                Width = 15,
+                Height = 10,
+                Color = "Red",
             };
-            Console.WriteLine(p);
+            Console.WriteLine(r);
             #endregion
         }
     }
